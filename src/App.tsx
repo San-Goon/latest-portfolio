bded23bd67be91c5c1e21248140e7090acb7fc4a
San-Goon <tigerlove95@naver.com>
2022-10-19 18:22:03 +0900

    feat: apply eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import SocialLinks from "./components/SocialLinks";
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import SocialLinks from './components/SocialLinks';
 
 function App() {
   return (
