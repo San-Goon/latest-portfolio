1802c4bccf807c34b51ce85cbd926db795e2b6a2
San-Goon <tigerlove95@naver.com>
2022-10-25 17:43:20 +0900

    refactor: delete header text & unnecessary font

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,10 +15,7 @@ const NavBar = () => {
     setNav((prev) => !prev);
   }, []);
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
-      <div>
-        <h1 className="text-5xl font-signature ml-2">Yash</h1>
-      </div>
+    <div className="flex flex-row-reverse justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <ul className="hidden md:flex">
         {LINKS.map(({ id, link }) => {
           return (
