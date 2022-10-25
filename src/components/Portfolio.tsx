0ac90596e7eddcdaae62665d029b103c203f86b1
San-Goon <tigerlove95@naver.com>
2022-10-25 21:21:53 +0900

    refactor: add my portfolios

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import dafalza from '../assets/portfolio/dafalza.png';
+import incourserun from '../assets/portfolio/incourserun.png';
+import stauter from '../assets/portfolio/stauter.png';
 
 const PROJECT_LIST = [
-  { id: 1, src: 'hi' },
-  { id: 2, src: 'hi' },
-  { id: 3, src: 'hi' },
-  { id: 4, src: 'hi' },
-  { id: 5, src: 'hi' },
-  { id: 6, src: 'hi' },
+  { id: 1, src: dafalza },
+  { id: 2, src: incourserun },
+  { id: 3, src: stauter },
+  // { id: 4, src: 'hi' },
 ];
 
 const Portfolio = () => {
@@ -20,25 +21,23 @@ const Portfolio = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className="py-6">Check out some of my work right here</p>
+          <p className="py-6">
+            현재까지 작업했던 프로젝트입니다. 클릭하시면 자세한 정보를 확인하실
+            수 있습니다.
+          </p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {PROJECT_LIST.map(({ id, src }) => {
             return (
-              <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <div
+                key={id}
+                className="flex shadow-md justify-center items-center shadow-gray-600 rounded-lg"
+              >
                 <img
                   src={src}
                   alt=""
                   className="rounded-md duration-200 hover:scale-105"
                 />
-                <div className="flex items-center justify-center">
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Demo
-                  </button>
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Code
-                  </button>
-                </div>
               </div>
             );
           })}
