46900e4ffd422b0b8132b57211ca0f3c458bf1a6
San-Goon <tigerlove95@naver.com>
2022-10-30 20:36:43 +0900

    chore: add underline

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -14,14 +14,18 @@ const Experience = () => {
         </div>
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-8 py-8 px-12 sm:px-0">
           <div>
-            <p className="text-2xl">Educations</p>
+            <div className="flex">
+              <p className="text-2xl border-b-4 border-gray-500">Educations</p>
+            </div>
             <p className="pt-6">안양대학교 정보전기전자공학과 졸업</p>
             <p className="text-gray-500 pt-2">2014.03~2021.02</p>
             <p className="pt-6">메가바이트스쿨 프론트엔드 과정 5기 수료</p>
             <p className="text-gray-500 pt-2">2022.07~2022.11</p>
           </div>
           <div>
-            <p className="text-2xl">Career</p>
+            <div className="flex">
+              <p className="text-2xl border-b-4 border-gray-500">Career</p>
+            </div>
             <p className="pt-6">라스트일마일</p>
             <p className="text-gray-500 pt-2">프론트엔드 개발자</p>
             <p className="text-gray-500">2021.11~2022.07</p>
